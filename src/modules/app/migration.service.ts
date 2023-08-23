4bd06afaddef13fbe551570167a102380d9e6a44
Ghazaleh <ghazaleh.khaki@appsfactory.de>
2023-08-23 16:44:51 +0200

    prepare initial migration

--- src/modules/app/migration.service.ts
+++ src/modules/app/migration.service.ts
@@ -2,18 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
 import { TermsConfig, TermsConfigDocument } from '../admin/schema/terms/terms-config.schema';
-import { Proposal, ProposalDocument } from '../proposal/schema/proposal.schema';
-import { KeycloakService } from '../user/keycloak.service';
 import { AppDbIdentifier } from './enums/app-db-identifier.enum';
-import {
-  Migration000,
-  Migration001,
-  Migration002,
-  Migration003,
-  Migration004,
-  Migration005,
-  Migration006,
-} from './migrations';
+import { Migration000 } from './migrations';
 import { Migration, MigrationDocument } from './schema/migration.schema';
 import { IDbMigration } from './types/db-migration.interface';
 import { DataPrivacyConfig, DataPrivacyConfigDocument } from '../admin/schema/data-privacy/data-privacy-config.schema';
@@ -26,11 +16,8 @@ export class MigrationService implements OnModuleInit {
     private migrationModel: Model<MigrationDocument>,
     @InjectModel(TermsConfig.name)
     private termsConfigModel: Model<TermsConfigDocument>,
-    @InjectModel(Proposal.name)
-    private proposalModel: Model<ProposalDocument>,
     @InjectModel(DataPrivacyConfig.name)
     private dataPrivacyConfigModel: Model<DataPrivacyConfigDocument>,
-    private keycloakService: KeycloakService,
   ) {}
 
   private readonly desiredDbVersion = 6;
@@ -39,13 +26,7 @@ export class MigrationService implements OnModuleInit {
   private readonly preventDowngrade = true;
 
   private readonly migrations: Record<number, IDbMigration> = {
-    0: new Migration000(this.migrationModel),
-    1: new Migration001(this.termsConfigModel),
-    2: new Migration002(this.proposalModel, this.keycloakService),
-    3: new Migration003(this.proposalModel),
-    4: new Migration004(this.proposalModel),
-    5: new Migration005(this.proposalModel),
-    6: new Migration006(this.proposalModel, this.dataPrivacyConfigModel),
+    0: new Migration000(this.migrationModel, this.termsConfigModel, this.dataPrivacyConfigModel),
   };
 
   private async runMigration(currentVersion?: number) {
@@ -58,7 +39,6 @@ export class MigrationService implements OnModuleInit {
       // Check for next migration
       await this.onModuleInit();
     } else if (currentVersion > this.desiredDbVersion && !this.preventDowngrade) {
-      await this.downgrade(currentVersion);
       // Check for next migration
       await this.onModuleInit();
     }
@@ -90,35 +70,6 @@ export class MigrationService implements OnModuleInit {
       throw error;
     } finally {
       session.endSession();
-    }
-  }
-
-  private async downgrade(currentVersion: number) {
-    const session = await this.connection.startSession();
-    session.startTransaction();
-
-    const migration = await this.migrationModel.findOne({ id: AppDbIdentifier.Migration });
-
-    const newVersion = currentVersion - 1;
-    console.log(`Downgrading DB to Version: ${newVersion}`);
-
-    try {
-      if (this.migrations[currentVersion]) {
-        await this.migrations[currentVersion].down();
-        migration.updatedAt = new Date();
-        migration.dbVersion = newVersion;
-        await migration.save();
-      } else {
-        const countOfMigrations = Object.keys(this.migrations).length;
-        throw new Error(
-          `Could not find migration file to downgrade for current version ${currentVersion}. Count of migrations: ${countOfMigrations}`,
-        );
-      }
-    } catch (error) {
-      await session.abortTransaction();
-      throw error;
-    } finally {
-      await session.endSession();
     }
   }
 
--- src/modules/app/migrations/index.ts
+++ src/modules/app/migrations/index.ts
@@ -1,7 +1,1 @@
 export { Migration000 } from './000.migration';
-export { Migration001 } from './001.migration';
-export { Migration002 } from './002.migration';
-export { Migration003 } from './003.migration';
-export { Migration004 } from './004.migration';
-export { Migration005 } from './005.migration';
-export { Migration006 } from './006.migration';
