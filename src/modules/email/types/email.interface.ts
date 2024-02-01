c917d7092d2c8ae744d889b69de0a4e49cca2e0e
Lars Schäfer <lars.schaefer@appsfactory.de>
2024-02-01 13:58:53 +0100

    Adds email templates

--- src/modules/email/types/email.interface.ts
+++ src/modules/email/types/email.interface.ts
@@ -2,8 +2,9 @@ import { EmailCategory } from './email-category.enum';
 
 export interface IEmailBase {
   to: string[];
-  subject: string;
   categories?: (EmailCategory | `ENV:${string}`)[];
 }
 
-export type IEmail = IEmailBase & ({ text: string } | { html: string });
+export type IEmail = IEmailBase & { subject: string } & ({ text: string } | { html: string });
+
+export type ITemplateEmail = IEmailBase & { subject?: string; templateId: number; params: Record<string, string> };
