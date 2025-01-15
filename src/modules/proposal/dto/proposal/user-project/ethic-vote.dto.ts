a3cd07dfa2f98c8b0f7e5a9f545b390baed87e44
Erwin Gronek <erwin.gronek@appsfactory.de>
2025-01-15 09:46:27 +0100

    [ZARS-703][ADD] remove ehtical votum checkbox (#34)

--- src/modules/proposal/dto/proposal/user-project/ethic-vote.dto.ts
+++ src/modules/proposal/dto/proposal/user-project/ethic-vote.dto.ts
@@ -1,5 +1,5 @@
 import { Expose, Type } from 'class-transformer';
-import { IsBoolean, IsDate, IsOptional, MaxLength, ValidateIf, Equals } from 'class-validator';
+import { IsBoolean, IsDate, IsOptional, MaxLength, ValidateIf } from 'class-validator';
 import { ProposalValidation } from 'src/modules/proposal/enums/porposal-validation.enum';
 import { WithIdForObjectDto } from 'src/shared/dto/with-id-for-object.dto';
 import { IsNotEmptyString } from 'src/shared/validators/is-not-empty-string.validator';
@@ -9,12 +9,6 @@ export class EthicVoteDto extends WithIdForObjectDto {
   @IsBoolean()
   @IsOptional({ groups: [ProposalValidation.IsDraft] })
   isExisting: boolean;
-
-  @Expose()
-  @IsOptional({ groups: [ProposalValidation.IsDraft] })
-  @ValidateIf((ethicVote: EthicVoteDto) => ethicVote.isExisting)
-  @Equals(true)
-  admitReputationOfAttachment: boolean;
 
   @Expose()
   @MaxLength(10000)
