6a5f1f5feee6f75aec50768d8bda7355324fdda1
Erwin Gronek <erwin.gronek@appsfactory.de>
2025-06-24 16:56:55 +0200

    ZARS-776 fix access of feasbility queries

--- src/modules/feasibility/feasibility.controller.ts
+++ src/modules/feasibility/feasibility.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Param, Request, Res, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Get, Request, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { ApiController } from 'src/shared/decorators/api-controller.decorator';
 import { Auth } from 'src/shared/decorators/auth.decorator';
@@ -6,8 +6,6 @@ import { Role } from 'src/shared/enums/role.enum';
 import { FdpgRequest } from 'src/shared/types/request-user.interface';
 import { FeasibilityUserQueryDetailDto } from './dto/feasibility-user-query-detail.dto';
 import { FeasibilityService } from './feasibility.service';
-import { NumberIdQueryIdDto } from 'src/shared/dto/uuid-id-param.dto';
-import { Response } from 'express';
 
 @Auth(Role.Researcher)
 @ApiController('feasibilities')
@@ -19,21 +17,5 @@ export class FeasibilityController {
   @ApiOperation({ summary: 'Returns all saved feasibility queries for the user' })
   getQueriesByUser(@Request() { user }: FdpgRequest): Promise<FeasibilityUserQueryDetailDto[]> {
     return this.feasibilityService.getQueriesByUser(user.userId);
-  }
-
-  @Get('query/:id/csv')
-  @UsePipes(ValidationPipe)
-  @ApiOperation({ summary: "Returns a queries zipped csv's" })
-  async getCsvByQueryId(@Param() { id }: NumberIdQueryIdDto, @Res() res: Response) {
-    const result = await this.feasibilityService.getQueryContentById(id, 'ZIP');
-
-    if (result && result instanceof Buffer) {
-      res.set({
-        'Content-Type': 'application/zip',
-      });
-      return res.send(result);
-    } else {
-      return res.status(204).send(); // No content
-    }
   }
 }
