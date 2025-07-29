9e2f3246b395ee272487a095e3754609843cc4cb
Elahe Karamzadeh <elahe.karamzadehmotlagh@appsfactory.de>
2025-07-29 14:05:12 +0200

    [HOT FIX] skip migration 13

--- src/modules/app/migrations/013.migration.ts
+++ src/modules/app/migrations/013.migration.ts
@@ -12,6 +12,7 @@ export class Migration013 implements IDbMigration {
   ) {}
 
   async up(): Promise<void> {
+    // This migration does not perform any operations. because it was created to handle a specific case that has already been resolved.
     console.log('Migration 013: No operation - skipping');
   }
 }
