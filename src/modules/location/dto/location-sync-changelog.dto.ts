b3e5847582e32daadc5f4b6287ef11c3400cbe6e
af-egr <erwin.gronek@appsfactory.de>
2025-10-28 12:18:30 +0100

    ZARS-766 add decorators to dto

--- src/modules/location/dto/location-sync-changelog.dto.ts
+++ src/modules/location/dto/location-sync-changelog.dto.ts
@@ -2,31 +2,45 @@ import { Expose, Type } from 'class-transformer';
 import { LocationSyncChangeLogStatus } from '../enum/location-sync-changelog-status.enum';
 import { LocationSyncChangelogStrategy } from '../enum/location-sync-changelog-strategy.enum';
 import { LocationDto } from './location.dto';
+import { IsDate, IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsNotEmptyString } from 'src/shared/validators/is-not-empty-string.validator';
 
 export class LocationSyncChangelogDto {
   @Expose()
+  @IsString()
+  @IsNotEmptyString()
   _id: string;
 
   @Expose()
+  @IsDate()
   created: Date;
 
   @Expose()
+  @IsEnum(LocationSyncChangeLogStatus)
   status: LocationSyncChangeLogStatus;
 
   @Expose()
+  @IsEnum(LocationSyncChangelogStrategy)
   strategy: LocationSyncChangelogStrategy;
 
   @Expose()
+  @IsString()
+  @IsNotEmptyString()
   forCode: string;
 
   @Expose()
+  @IsString()
+  @IsOptional()
   statusSetBy?: string;
 
   @Expose()
+  @IsDate()
+  @IsOptional()
   statusSetDate?: Date;
 
   @Expose()
   @Type(() => LocationDto)
+  @IsOptional()
   oldLocationData?: Omit<LocationDto, 'rubrum'>;
 
   @Expose()
--- src/modules/location/dto/location.dto.ts
+++ src/modules/location/dto/location.dto.ts
@@ -1,16 +1,70 @@
+import { Expose } from 'class-transformer';
+import { IsBoolean, IsDate, IsOptional, IsString } from 'class-validator';
+import { IsNotEmptyString } from 'src/shared/validators/is-not-empty-string.validator';
+
 export class LocationDto {
+  @Expose()
+  @IsString()
+  @IsNotEmptyString()
   _id: string;
+
+  @Expose()
+  @IsString()
+  @IsNotEmptyString()
   externalCode: string;
+
+  @Expose()
+  @IsString()
+  @IsNotEmptyString()
   display: string;
+
+  @Expose()
+  @IsString()
+  @IsOptional()
   definition?: string;
+
+  @Expose()
+  @IsString()
+  @IsNotEmptyString()
   consortium: string;
+
+  @Expose()
+  @IsString()
+  @IsOptional()
   contract?: string;
+
+  @Expose()
+  @IsString()
+  @IsOptional()
   abbreviation?: string;
+
+  @Expose()
+  @IsString()
+  @IsOptional()
   uri?: string;
+
+  @Expose()
+  @IsString()
+  @IsOptional()
   rubrum?: string;
+
+  @Expose()
+  @IsBoolean()
   dataIntegrationCenter: boolean;
+
+  @Expose()
+  @IsString()
+  @IsBoolean()
   dataManagementCenter: boolean;
+
+  @Expose()
+  @IsDate()
+  @IsOptional()
   deprecationDate?: Date;
+
+  @Expose()
+  @IsBoolean()
+  @IsOptional()
   deprecated: boolean;
 }
 
