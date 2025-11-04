cd183bb98b8c6e04736cf015216e0ad3b10b9e55
Mirco Steyer <mirco.steyer@appsfactory.de>
2025-11-04 13:23:34 +0100

    feat: #1129 parent ticket #1065 initial setup of delivery data for data mangement overview

---
+++ src/modules/proposal/dto/proposal/data-delivery/data-delivery.dto.ts
@@ -0,0 +1,41 @@
+import { Expose, Type } from 'class-transformer';
+import { IsEnum, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { DeliveryAcceptance } from '../../../enums/data-delivery.enum';
+import { DeliveryInfoGetDto, DeliveryInfoUpdateDto } from './delivery-info.dto';
+
+export class DataDeliveryUpdateDto {
+  @Expose()
+  @IsString()
+  dataManagementSite: string;
+
+  @Expose()
+  @IsEnum(DeliveryAcceptance)
+  acceptance: DeliveryAcceptance;
+
+  @Expose()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DeliveryInfoUpdateDto)
+  delivery?: DeliveryInfoUpdateDto | null;
+}
+
+export class DataDeliveryGetDto {
+  @Expose()
+  dataManagementSite: string;
+
+  @Expose()
+  acceptance: DeliveryAcceptance;
+
+  @Expose()
+  @Type(() => DeliveryInfoGetDto)
+  @IsOptional()
+  delivery?: DeliveryInfoGetDto | null;
+
+  @Expose()
+  @IsOptional()
+  createdAt?: Date;
+
+  @Expose()
+  @IsOptional()
+  updatedAt?: Date;
+}
