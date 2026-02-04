d0fa899f58da4c8130534aa31611b4114b57c0a6
af-egr <erwin.gronek@appsfactory.de>
2026-02-04 12:47:46 +0100

    ZARS-1238 fix upload delete validation

--- src/modules/proposal/utils/validate-upload-deletion.util.ts
+++ src/modules/proposal/utils/validate-upload-deletion.util.ts
@@ -19,26 +19,27 @@ const generalAccessTypes = [
 export const validateUploadDeletion = (proposal: ProposalDocument, upload: Upload, user: IRequestUser) => {
   const hasRegisteringMemberRole = user.roles.includes(Role.RegisteringMember);
 
-  if (hasRegisteringMemberRole) {
-    checkForRegisteringMember(proposal, upload, user);
-    return;
-  }
-
-  if (user.singleKnownRole === Role.Researcher) {
-    checkForResearcher(proposal, upload, user);
+  if (user.singleKnownRole === Role.Researcher && proposal.type === ProposalType.ApplicationForm) {
+    return checkForResearcher(proposal, upload, user);
   }
 
   if (user.singleKnownRole === Role.FdpgMember || user.singleKnownRole === Role.DataSourceMember) {
-    checkForFdpgMember(proposal);
+    return checkForFdpgMember(proposal);
   }
 
   if (user.singleKnownRole === Role.DizMember) {
-    checkForLocationMember(proposal, upload);
+    return checkForLocationMember(proposal, upload);
   }
 
   if (user.singleKnownRole === Role.UacMember) {
-    checkForLocationMember(proposal, upload);
+    return checkForLocationMember(proposal, upload);
   }
+
+  if (hasRegisteringMemberRole && proposal.type === ProposalType.RegisteringForm) {
+    return checkForResearcher(proposal, upload, user);
+  }
+
+  throwForbiddenError();
 };
 
 const checkForResearcher = (proposal: ProposalDocument, upload: Upload, user: IRequestUser) => {
@@ -75,14 +76,6 @@ const checkForResearcher = (proposal: ProposalDocument, upload: Upload, user: IR
     !isEditable &&
     isEthicSectionDone
   ) {
-    throwForbiddenError();
-  }
-};
-
-const checkForRegisteringMember = (proposal: ProposalDocument, upload: Upload, user: IRequestUser) => {
-  if (proposal.type === ProposalType.RegisteringForm) {
-    checkForResearcher(proposal, upload, user);
-  } else {
     throwForbiddenError();
   }
 };
